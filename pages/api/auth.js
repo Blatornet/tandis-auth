b6deb1c240839a41dbbbe9a95eda18b62eb82573
Anders Friborg <frippera@gmail.com>
2024-02-06 11:41:49 +0100

    partial commit auth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,11 +1,52 @@
 const jwt = require("../../utils/jwt");
+const axios = require("axios");
+const https = require("https");
+const qs = require("qs");
 
-const auth = (req, res) => {
-    const accessToken = jwt.sign({ sub: "bb@blatornet.se" });
+const { RSSO_AUTH_AUDIENCE, RSSO_URL, RSSO_AUTH_CLIENT_TOKEN } = process.env;
 
-    return res.json({
-      accessToken,
-    })
+const auth = async (req, res) => {
+  const userId = "bb@blatornet.se";
+
+  const accessToken = jwt.sign({ sub: userId });
+
+  // Create Agent to fix SSL error due to internal certificate
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
+  // Create Payload
+  const data = qs.stringify({
+    grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+    assertion: accessToken.toString(),
+    audience: RSSO_AUTH_AUDIENCE,
+  });
+
+  // Create Axios conf
+  const httpCall = {
+    method: "post",
+    url: RSSO_URL + "/rsso/oauth2/token",
+    headers: {
+      Authorization: "Basic " + RSSO_AUTH_CLIENT_TOKEN,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    httpsAgent: httpsAgent,
+    data: data.toString(),
+  };
+
+  try {
+    // Get Authentication Response
+    const response = await axios(httpCall);
+
+    //console.log(data);
+    //console.log("###");
+    //console.log(httpCall);
+  } catch (error) {
+    // Log Error
+    console.log(error);
   }
+
+  return res.json({
+    accessToken,
+  });
+}
 
   export default auth;
--- pages/bankid/anotherdevice.js
+++ pages/bankid/anotherdevice.js
@@ -1,17 +1,29 @@
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 const AnotherDevice = () => {
   const router = useRouter();
+
+  const handleLogin = async () => {
+    const response = await axios("/api/auth");
+    const token = response.data.accessToken;
+
+    
+  }
 
   return (
     <div>
       <h1>Another Device</h1>
 
+      <button type="button" onClick={handleLogin}>
+        Log in
+      </button>
+
       <button type="button" onClick={() => router.back()}>
         Cancel
       </button>
     </div>
-  )
+  );
 }
 
 export default AnotherDevice;
